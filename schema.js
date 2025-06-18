026458569ec5c935bfa3f3ac203798ded117563f
Pratyush Kumar <pratyushsharma978@gmail.com>
2025-06-18 09:55:07 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,12 +1,4 @@
-// const Joi = require('joi');
-// module.exports.listingSchema = Joi.object({
-//   title: Joi.string().required(),
-//   description: Joi.string().required(),
-//   price: Joi.number().required().min(0),
-//   location: Joi.string().required(),
-//   country: Joi.string().required(),
-//   image: Joi.string().uri().allow('',null) // <-- Add this line
-// });
+
 
 const Joi = require('joi');
 
